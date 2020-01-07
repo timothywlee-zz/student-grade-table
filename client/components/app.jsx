92c37827e8587ee4e03bcd33e7f4a4aba8d800da
Timothy Lee <timothylee3508@gmail.com>
2020-01-06 16:10:58 -0800

    testing

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Header from './header';
+import GradeTable from './grade-table';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { grades: [] };
+    this.getAllGrades = this.getAllGrades.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllGrades();
+  }
+
+  getAllGrades() {
+    fetch('/api/grades')
+      .then(response => { return response.json(); })
+      .then(data => { this.setState({ grades: data }); })
+      .catch(err => { console.log('An error occured: ', err); });
+  }
+
   render() {
-    return null;
+    return (
+      <>
+        <Header />
+        <GradeTable grades={this.state.grades} />
+      </>
+    );
   }
 }
-
-export default App;
